09971a67004d72c1a1a5fe17ba0976ba8621fbf3
kurrrru <kurrrru8850@gmail.com>
2024-12-05 21:12:57 +0900

    コンパイル時の警告をprintfにあわせた

--- ft_printf.h
+++ ft_printf.h
@@ -17,6 +17,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# if defined(__GNUC__) || defined(__clang__)
+#  define PRINTF_ATTRIBUTE(a, b) __attribute__((format(printf, a, b)))
+# else
+#  define PRINTF_ATTRIBUTE(a, b)
+# endif
+
 int	ft_printf_c(char c);
 int	ft_printf_s(char *s);
 int	ft_printf_d(int d);
@@ -24,6 +30,6 @@ int	ft_printf_u(unsigned int u);
 int	ft_printf_x(unsigned int x);
 int	ft_printf_x_upper(unsigned int x);
 int	ft_printf_p(void *p);
-int	ft_printf(const char *format, ...);
+int	ft_printf(const char *format, ...) PRINTF_ATTRIBUTE(1, 2);
 
 #endif
